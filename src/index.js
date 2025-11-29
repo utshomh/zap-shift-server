78a9363f88c85d0f269c14b8db61aad83ce8383e
Utsho MH <utshomh.dev@gmail.com>
2025-11-29 23:06:27 +0600

    added payment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
+import Stripe from "stripe";
 
 const app = express();
 
@@ -45,14 +46,73 @@ client
 const database = client.db("zap-shift");
 const parcelsCollection = database.collection("parcels");
 
-// API Routes
+const stripe = new Stripe(process.env.STRIPE_SECRET);
+
+// General Routes
 app.get("/", (_req, res) => {
   res.json({ message: "Welcome to Zap Shift API" });
 });
 
+// Payment Routes
+app.post("/payment-checkout", async (req, res) => {
+  try {
+    const parcel = req.body;
+    const amount = parcel.charge * 100;
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: "bdt",
+            unit_amount: amount,
+            product_data: {
+              name: parcel.parcelName,
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      metadata: {
+        parcelId: parcel._id,
+      },
+      customer_email: parcel.senderEmail,
+      success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success?sessionId={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+    });
+
+    res.json({ url: session.url });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.patch("/verify-payment", async (req, res) => {
+  try {
+    const sessionId = req.query.sessionId;
+    const session = await stripe.checkout.sessions.retrieve(sessionId);
+    const paid = session.payment_status === "paid";
+
+    if (paid) {
+      const filter = { _id: new ObjectId(session.metadata.parcelId) };
+      const update = {
+        $set: {
+          paymentStatus: "paid",
+        },
+      };
+      const updatedParcel = await parcelsCollection.updateOne(filter, update);
+      res.json({ paid, ...updatedParcel });
+    } else {
+      res.json({ paid });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Parcel Routes
 app.get("/parcels", async (req, res) => {
   try {
-    const allowedFields = ["email"];
+    const allowedFields = ["senderEmail"];
     const query = {};
 
     for (const key of allowedFields) {
@@ -61,7 +121,6 @@ app.get("/parcels", async (req, res) => {
       }
     }
 
-    // Sorting support
     const sortField = req.query.sort || "createdAt";
     const sortOrder = req.query.order === "asc" ? 1 : -1;
 
@@ -71,6 +130,21 @@ app.get("/parcels", async (req, res) => {
       .toArray();
 
     res.json(parcels);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/parcels/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const query = { _id: new ObjectId(id) };
+    const parcel = await parcelsCollection.findOne(query);
+    if (parcel) {
+      res.json(parcel);
+    } else {
+      res.status(404).json({ message: "Parcel Not Found" });
+    }
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
@@ -102,6 +176,7 @@ app.delete("/parcels/:id", async (req, res) => {
   }
 });
 
+// Error Routes
 app.all(/.*/, (_req, res) => {
   res.status(404).json({
     message: "Route Not Found",
